a6afe12303f09cdad5577eb461b0645d0f3c85b9
Aljoboyer <tanveeazmain@gmail.com>
2024-04-19 16:27:37 +0600

    layout structure done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <Route path="/" element={<LandingHome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
-          
         </Routes>
       </Router>
     </main>
