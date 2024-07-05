952c783f1a5a2bbcae85c130974e91795f2480b1
Aljoboyer <tanveeazmain@gmail.com>
2024-07-05 10:37:47 +0600

    done

--- src/components/admin/Studio/ScreenCreateModal.jsx
+++ src/components/admin/Studio/ScreenCreateModal.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
+import AndroidLogo from '../../../assets/image/Potrait.png'
+import Chromelandscape from '../../../assets/image/landchrome.png'
+import Firetvlogo from '../../../assets/image/firetvlogo.png'
 
 const ScreenCreateModal = ({ visible, onClose}) => {
 
@@ -13,7 +16,7 @@ const ScreenCreateModal = ({ visible, onClose}) => {
         <div
             id="ScreenCreateModal"
             onClick={toggleModal}
-            className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center text-left w-full h-full outline-none overflow-auto z-10 px-4"
+            className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center text-left w-full h-full outline-none overflow-auto z-10 px-4 "
         >
             <div className="h-auto p-4 bg-white rounded-lg xs:mx-3 md:w-2/3 w-full ">
                <div className='flex flex-row justify-between items-center border-b-2 border-gray-200 pb-4'>
@@ -28,7 +31,7 @@ const ScreenCreateModal = ({ visible, onClose}) => {
                     </div>
                </div>
                
-               <p className='text-lg font-medium  mt-2'>Screen Name</p>
+               <p className='text-lg font-medium  mt-4'>Screen Name</p>
                
                <div className='w-full xl:flex xl:flex-row lg:flex lg:flex-row  md:flex md:flex-row sm:flex sm:flex-col  items-center mt-2'>
                    <div className='w-1/2'>
@@ -36,7 +39,7 @@ const ScreenCreateModal = ({ visible, onClose}) => {
                    </div>
                    <div className='flex justify-center items-center ms-4 w-1/2'>
                         <input className='outline-none border border-1 border-gray-400 rounded-md px-2 h-[40px] md:w-2/3 w-full' placeholder='Enter tag' />
-                        <button className='px-4 py-2 rounded-md text-white bg-blue-400'>Create Tag</button>
+                        <button className='px-4 py-2 rounded-md text-white bg-blue-500'>Create Tag</button>
                    </div>
                </div>
                 
@@ -51,10 +54,26 @@ const ScreenCreateModal = ({ visible, onClose}) => {
                     </div>
                </div>
 
-                <div className='mt-4'>
-                    <div className='xl:flex xl:flex-row lg:flex lg:flex-row  md:flex md:flex-col sm:flex sm:flex-col w-full'>
-                    
+                <div className='mt-9'>
+                    <p className='font-bold text-xl'>Choose Screen Type</p>
+                    <div className='xl:flex xl:flex-row lg:flex lg:flex-row  md:flex md:flex-col sm:flex sm:flex-col w-full justify-evenly mt-4'>
+                        <div>
+                            <input type="checkbox"/>
+                            <img src={AndroidLogo} className='' />
+                        </div>
+                        <div>
+                            <input type="checkbox"/>
+                            <img src={Chromelandscape} className='w-[280px]' />
+                        </div>
+                        <div>
+                            <input type="checkbox"/>
+                            <img src={Firetvlogo} className='w-[280px]'  />
+                        </div>
                     </div>
+                   
+                   <div className='flex justify-center py-4'>
+                        <button className='w-[250px] h-[50px] text-center bg-blue-500 text-white rounded-md font-medium text-[16px]'>Submit</button>
+                   </div>
                 </div>
             </div>
         </div>
